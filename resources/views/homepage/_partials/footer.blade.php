77b182222090e243f8e982cbbadcd1a920b3d060
Rahmawati Nurul Janah <63674912+rahmawnj@users.noreply.github.com>
2026-09-05 21:43:26 +0700

    Fix Blade syntax error in footer social media markup

--- resources/views/homepage/_partials/footer.blade.php
+++ resources/views/homepage/_partials/footer.blade.php
@@ -4,9 +4,8 @@
         <div class="container">
             <div class="row d-flex justify-content-between">
                 <div class="col-12">
-                    <!-- logo -->
                     <div class="footer-logo mb-20">
-                    <a href="/"><img height="50" src="{{asset('storage/' . \\App\Models\Content::where('name', 'logo_footer')->first()->description)}}" alt=""></a>
+                        <a href="/"><img height="50" src="{{asset('storage/' . App\Models\Content::where('name', 'logo_footer')->first()->description)}}" alt=""></a>
                     </div>
                 </div>
                 <div class="col-lg-6 col-xs-12">
@@ -15,17 +14,14 @@
                             <h4>Contact Info</h4>
                             <ul>
                                 <li>
-
-                                <p class="text-white">Address : 
-                                    <a style="color: white;" href="https://www.google.com/maps/place/{{\\App\Models\Content::where('name', 'address')->first()->description}}">{{\\App\Models\Content::where('name', 'address')->first()->description}}</a>
-
+                                    <p class="text-white">Address :
+                                        <a style="color: white;" href="https://www.google.com/maps/place/{{App\Models\Content::where('name', 'address')->first()->description}}">{{App\Models\Content::where('name', 'address')->first()->description}}</a>
                                     </p>
                                 </li>
-                                <li><a class="text-white" href="https://wa.me/{{\\App\Models\Content::where('name', 'phone')->first()->description}}">Phone : {{\\App\Models\Content::where('name', 'phone')->first()->description}}</a></li>
-                                <li><a class="text-white" href="mailto:{{\\App\Models\Content::where('name', 'email')->first()->description}}">Email : {{\\App\Models\Content::where('name', 'email')->first()->description}}</a></li>
+                                <li><a class="text-white" href="https://wa.me/{{App\Models\Content::where('name', 'phone')->first()->description}}">Phone : {{App\Models\Content::where('name', 'phone')->first()->description}}</a></li>
+                                <li><a class="text-white" href="mailto:{{App\Models\Content::where('name', 'email')->first()->description}}">Email : {{App\Models\Content::where('name', 'email')->first()->description}}</a></li>
                             </ul>
                         </div>
-
                     </div>
                 </div>
                 <div class="col-lg-6 col-xs-12">
@@ -44,29 +40,33 @@
             </div>
         </div>
     </div>
+
     <!-- footer-bottom area -->
     <div class="footer-bottom-area footer-bg" style="background-color: #1e214e;">
         <div class="container">
             <div class="footer-border">
-                 <div class="row d-flex justify-content-between align-items-center">
-                     <div class="col-xl-10 col-lg-10 ">
-                         <div class="footer-copy-right">
-                             <p class="text-white"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> {{\\App\Models\Content::where('name', 'name')->first()->description}}
-                                <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
-                         </div>
-                     </div>
-                     <div class="col-xl-2 col-lg-2">
-                         <div class="footer-social f-right footer-social-consistent">
-                            @foreach (\\App\Models\Mediasocial::where('status', 'active')->get() as $mediasocial)
-                            <a class="site-social-link" title="{{$mediasocial->name}}" href="{{$mediasocial->link}}" target="_blank" rel="noopener noreferrer"><i class="fa-brands fa-{{$mediasocial->icon}}"></i></a>
+                <div class="row d-flex justify-content-between align-items-center">
+                    <div class="col-xl-10 col-lg-10">
+                        <div class="footer-copy-right">
+                            <p class="text-white">
+                                Copyright &copy;<script>document.write(new Date().getFullYear());</script> {{App\Models\Content::where('name', 'name')->first()->description}}
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-2">
+                        <div class="footer-social f-right footer-social-consistent">
+                            @foreach (App\Models\Mediasocial::where('status', 'active')->get() as $mediasocial)
+                                <a class="site-social-link" title="{{$mediasocial->name}}" href="{{$mediasocial->link}}" target="_blank" rel="noopener noreferrer">
+                                    <i class="fa-brands fa-{{$mediasocial->icon}}"></i>
+                                </a>
                             @endforeach
-                         </div>
-                     </div>
-                 </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+
     <style>
         .footer-social-consistent {
             display: flex !important;
